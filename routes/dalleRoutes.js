fd5a68dcc8ad7796d97a1b6d120400297887ed1d
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2023-12-25 22:43:15 +0530

    chores:no use after free trial

--- routes/dalleRoutes.js
+++ routes/dalleRoutes.js
@@ -14,4 +14,22 @@ router.route("/").get((req, res) => {
 	res.send("Hello from DALL-E");
 });
 
+router.route("/").post(async (req, res) => {
+	try {
+		const { prompt } = req.body;
+		const aiRes = await openai.images.generate({
+			model: "dall-e-3",
+			prompt,
+			n: 1,
+			size: "1024x1024",
+			response_format: "b64_json",
+		});
+		const image = aiRes.data[0]?.b64_json;
+		res.status(200).json({ photo: image });
+	} catch (error) {
+		console.log(error);
+		res.status(500).send(error?.response?.data.error.message);
+	}
+});
+
 export default router;
