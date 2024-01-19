6f8dba05376fffe7add53cfa0fb2a5fa264d2048
pawanpdn-671 <pawanpradhanpc671@gmail.com>
2024-01-19 18:44:18 +0530

    update:debugging error4

--- app/games/flappy-bird/page.tsx
+++ app/games/flappy-bird/page.tsx
@@ -1,10 +1,20 @@
+import { Metadata } from "next";
 import dynamic from "next/dynamic";
+
+export const metadata: Metadata = {
+	title: "Flappy Bird | PawanPdn",
+	description: "Play Flappy Bird in your mobile or pc.",
+	icons: [
+		{ rel: "icon", url: "/icon.ico" },
+		{ rel: "apple", url: "/icon.ico" },
+	],
+};
 
 const FlappyBird = dynamic(() => import("../_components/FlappyBird"), {
 	ssr: false,
 });
 
 const Page = () => {
-	return typeof window !== "undefined" ? <FlappyBird /> : <></>;
+	return <FlappyBird />;
 };
 export default Page;
--- app/games/space-invaders/page.tsx
+++ app/games/space-invaders/page.tsx
@@ -1,10 +1,20 @@
 import dynamic from "next/dynamic";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+	title: "Space Invaders | PawanPdn",
+	description: "Play Space Invaders in your pc.",
+	icons: [
+		{ rel: "icon", url: "/icon.ico" },
+		{ rel: "apple", url: "/icon.ico" },
+	],
+};
 
 const SpaceInvaders = dynamic(() => import("../_components/SpaceInvaders"), {
 	ssr: false,
 });
 
 const Page = () => {
-	return typeof window !== "undefined" ? <SpaceInvaders /> : <></>;
+	return <SpaceInvaders />;
 };
 export default Page;
